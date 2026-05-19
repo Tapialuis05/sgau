2e885506cc75f080636cc40bafad9955cc1096e4
Luis Tapia <tapialuiseduardo13@gmail.com>
2026-05-19 10:36:09 -0500

    fix: remover wait-for-db.sh de todos los servicios

--- grades_service/app/db.py
+++ grades_service/app/db.py
@@ -2,7 +2,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:123456@db:5432/grades_db")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
